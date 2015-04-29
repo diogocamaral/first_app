ca4c036c6276c32063dfa52c611dd2cccfaee0a6
Diogo Amaral <diogo@Diogos-MacBook-Pro.local>
2015-04-28 21:27:26 -0300

    first commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Diogo Amaral on 4/25/15.
+//  Copyright (c) 2015 Diogo Amaral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
